ee52b7a89caefdb3200d6682b93d4e114427a22d
mrhid3 <gkornas@proton.me>
2026-05-17 17:01:07 +0200

    started repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<h1 class="prose prose-2xl">Forms</h1>
+<p class="prose prose-gray">A new way to form</p>
+<button class="">Get started</button>
+
+<style>
+    @import "tailwindcss";
+</style>
